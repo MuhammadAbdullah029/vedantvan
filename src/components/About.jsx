a682e28d77f0703f92e68b5bcf74aebbff66a5cb
Muhammad Abdullah <abdullahgu77ar@gmail.com>
2025-09-26 19:23:18 +0500

    about img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TextEff from './TextEff'
+import DevImg from '../assets/Dev.jpg'
 
 const About = () => {
   return (
@@ -7,7 +8,7 @@ const About = () => {
        <div className="w-[100%] h-[100vh] p-[3rem] ">
         <div className="w-[100%] h-[100%] border-[1px] border-[#c5c4c4] rounded-[25px] flex items-center justify-between p-[1vw] ">
           <div className="left w-[49%] h-[100%] rounded-[15px] overflow-hidden ">
-            <img className='w-[65%] h-full object-cover rounded-[15px] m-auto ' src="src/assets/Dev.jpg" alt="" />
+            <img className='w-[65%] h-full object-cover rounded-[15px] m-auto ' src={DevImg} alt="" />
           </div>
           <div className="right w-[49%] h-[100%] rounded-[15px] flex flex-col justify-between p-[1rem] ">
             <h1 className='text-4xl font-bold ' >About Me</h1>
