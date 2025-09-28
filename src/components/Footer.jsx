03c3603e067b9eb99ef3b41e8b5a4386c310e9ce
Muhammad Abdullah <abdullahgu77ar@gmail.com>
2025-09-28 20:15:28 +0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,26 +4,26 @@ import { Mail } from "lucide-react";
 const Footer = () => {
   return (
     <footer className="w-full mt-16 p-[3rem] pb-[1rem] ">
-      <div className="w-full bg-[#F7F6F6] p-[1rem] rounded-[15px] ">
+      <div className="w-full bg p-[1rem] rounded-[15px] ">
         <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
         
         {/* Left side - Branding */}
         <div className="text-center md:text-left">
-          <h2 className="text-3xl font-bold text-gray-800 cur ">Vedant Vanmore</h2>
-          <p className="text-sm text-gray-600 mt-2">
+          <h2 className="text-3xl font-bold white cur ">Vedant Vanmore</h2>
+          <p className="text-sm white mt-2">
             Helping businesses scale with AI, automation, and efficient workflows.
           </p>
         </div>
 
         {/* Center - Links */}
-        <nav className="flex gap-6 text-gray-700 text-sm font-medium">
-          <a href="#services" className="hover:text-gray-900 transition-colors">
+        <nav className="flex gap-6 white text-sm font-medium">
+          <a href="#services" className="hover:scale-105 white ">
             Services
           </a>
-          <a href="#about" className="hover:text-gray-900 transition-colors">
+          <a href="#about" className="hover:scale-105 white ">
             About
           </a>
-          <a href="#contact" className="hover:text-gray-900 transition-colors">
+          <a href="#contact" className="hover:scale-105 white ">
             Contact
           </a>
         </nav>
@@ -41,7 +41,7 @@ const Footer = () => {
       </div>
 
       {/* Bottom line */}
-      <div className="mt-10 border-t border-gray-300 pt-4 text-center text-sm text-gray-600">
+      <div className="mt-10 border-t border-gray-300 pt-4 text-center text-sm white">
         © {new Date().getFullYear()}. All rights reserved.
       </div>
       </div>
